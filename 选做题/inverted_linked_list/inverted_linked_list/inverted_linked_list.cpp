cc9b5718e36067ad60c13ab8a736ed9983cf9f1c
zwx003 <128168163+zwx003@users.noreply.github.com>
2023-03-19 22:04:37 +0800

    Add files via upload

---
+++ 选做题/inverted_linked_list/inverted_linked_list/inverted_linked_list.cpp
@@ -0,0 +1,61 @@
+﻿#include<stdio.h>
+typedef struct Node
+{
+	int data;
+	struct Node* next;
+}Node, * List;
+
+int i;
+
+void input(List& head)
+{
+	head = new Node;
+	head->next = NULL;
+	List p, r;
+	r = head;
+	printf("请输入你要输入几个数据:");
+	scanf_s("%d", &i);
+	while (i)
+	{
+		p = new Node;
+		scanf_s("%d", &p->data);
+		r->next = p;
+		p->next = NULL;
+		r = p;
+		i--;
+	}
+}
+
+void reservelist(List& head)
+{
+	List p, r, next=NULL;
+	r = NULL;
+	p = head->next;
+	while (p) 
+	{
+		next = p->next;
+		p->next = r;
+		r = p;
+		p = next;
+	}
+	head= r;
+}
+
+void output(List& head)
+{
+	List p;
+	p = head;
+	while (p)
+	{
+		printf("%d ", p->data);
+		p = p->next;
+	}
+}
+
+int main()
+{
+	List head;
+	input(head);
+	reservelist(head);
+	output(head);
+}
---
+++ 选做题/middle_list/middle_list/middle_list.cpp
@@ -0,0 +1,56 @@
+﻿#include<stdio.h>
+typedef struct Node
+{
+	int data;
+	struct Node* next;
+}Node,*List;
+
+int i;
+
+void input(List& head)
+{
+	head = new Node;
+	head->next = NULL;
+	List p, r;
+	r = head;
+	printf("请输入你要输入几个数据:");
+	scanf_s("%d", &i);
+	while (i)
+	{
+		p = new Node;
+		scanf_s("%d", &p->data);
+		r->next = p;
+	    p->next=NULL;
+		r = p;
+		i--;
+	}
+}
+
+void middle(List&head)
+{
+	int n=0;
+	List fast, slow;
+	fast = head;
+	slow = head;
+	while (fast)
+	{
+		if (fast->next == NULL)
+		{
+			printf("中间节点为两个节点:");
+			printf("%d %d", slow->data, slow->next->data);
+		}
+	    fast = fast->next->next;
+		slow = slow->next;
+	}
+	
+		printf("中间节点为:");
+		printf("%d", slow->data);
+}
+
+
+int main()
+{
+	List head;
+	input(head);
+	middle(head);
+}
